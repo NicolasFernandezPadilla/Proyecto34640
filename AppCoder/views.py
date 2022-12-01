2ede93ba8fb2c074a866b00a51aabd79650647a9
NicolasFernandezPadilla <nicofpadilla@gmail.com>
2022-12-01 20:08:24 -0300

    fin clase 18

---
+++ AppCoder/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from .models import Curso
+from django.http import HttpResponse
+
+# Create your views here.
+
+def curso(request):
+
+    curso=Curso(nombre="Python",comision=34640)
+    curso.save()
+    cadena_Texto="Curso guardado: "+curso.nombre+" "+str(curso.comision)
+    return HttpResponse(cadena_Texto)
