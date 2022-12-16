1687173cdeb504fa120c18f99f1ed69d1eaaa330
NicolasFernandezPadilla <nicofpadilla@gmail.com>
2022-12-16 18:14:42 -0300

    fin clase 20

--- AppCoder/views.py
+++ AppCoder/views.py
@@ -7,6 +7,22 @@ from django.http import HttpResponse
 def curso(request):
 
     curso=Curso(nombre="Python",comision=34640)
+
     curso.save()
     cadena_Texto="Curso guardado: "+curso.nombre+" "+str(curso.comision)
     return HttpResponse(cadena_Texto)
+
+def inicio(request):
+    return render(request, "AppCoder/inicio.html")
+ 
+def cursos(request):
+    return render(request, "AppCoder/cursos.html")
+
+def estudiantes(request):
+    return render(request, "AppCoder/estudiantes.html")
+
+def profesores(request):
+    return render(request, "AppCoder/profesores.html")
+
+def entregables(request):
+    return render(request, "AppCoder/entregables.html")
